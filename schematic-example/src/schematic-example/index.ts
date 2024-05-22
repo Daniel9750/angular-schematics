2167575ddb2e433f824dbdf07fb52e5a0e929a90
Daniel <danielsanchezgonzalez@alumno.ieselrincon.es>
2024-05-22 15:26:48 +0100

    schematic example added

---
+++ schematic-example/src/schematic-example/index.ts
@@ -0,0 +1,13 @@
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+
+
+// You don't have to export the function as default. You can also have more than one rule factory
+// per file.
+
+  console.log('Hello world');
+
+export function schematicExample(_options: any): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    return tree;
+  };
+}
